64e0806f404aefb605aebf23cbc2d5dba65728de
Nil Geisweiller <ngeiswei@gmail.com>
2020-06-06 16:14:25 +0300

    Fix compiling errors and add missing parameters to DSSI

--- src/dssizynayumi.cpp
+++ src/dssizynayumi.cpp
@@ -52,11 +52,11 @@ void DSSIZynayumi::run_synth(unsigned long sample_count,
 			switch(events[e].type) {
 				//Note On, Off
 			case SND_SEQ_EVENT_NOTEON :
-				zynayumi.noteOn_process(0, events[e].data.note.note,
-				                        events[e].data.note.velocity);
+				zynayumi.note_on_process(0, events[e].data.note.note,
+				                         events[e].data.note.velocity);
 				break;
 			case SND_SEQ_EVENT_NOTEOFF :
-				zynayumi.noteOff_process(0, events[e].data.note.note);
+				zynayumi.note_off_process(0, events[e].data.note.note);
 				break;
 				//Sysex
 			case SND_SEQ_EVENT_SYSEX :
@@ -116,6 +116,8 @@ void initialise_2() {
 	const LADSPA_PortDescriptor c_desc = LADSPA_PORT_CONTROL | LADSPA_PORT_INPUT;
 	const LADSPA_PortRangeHintDescriptor r_desc = LADSPA_HINT_BOUNDED_BELOW |
 		LADSPA_HINT_BOUNDED_ABOVE;
+	const LADSPA_PortRangeHintDescriptor l_desc = LADSPA_HINT_BOUNDED_BELOW |
+		LADSPA_HINT_BOUNDED_ABOVE | LADSPA_HINT_LOGARITHMIC;
 	const LADSPA_PortRangeHintDescriptor i_desc = LADSPA_HINT_BOUNDED_BELOW |
 		LADSPA_HINT_BOUNDED_ABOVE | LADSPA_HINT_INTEGER;
 	const LADSPA_PortRangeHintDescriptor t_desc = LADSPA_HINT_TOGGLED;
@@ -126,129 +128,143 @@ void initialise_2() {
 	const LADSPA_PortRangeHintDescriptor d_high = LADSPA_HINT_DEFAULT_HIGH;
 	const LADSPA_PortRangeHintDescriptor d_0 = LADSPA_HINT_DEFAULT_0;
 	const LADSPA_PortRangeHintDescriptor d_1 = LADSPA_HINT_DEFAULT_1;
+	const LADSPA_PortRangeHintDescriptor d_100 = LADSPA_HINT_DEFAULT_100;
+	const LADSPA_PortRangeHintDescriptor d_440 = LADSPA_HINT_DEFAULT_440;
+
+	const double delta = 0.1;
+
+	// Emulation mode
+	ports.add_port(c_desc, EMUL_MODE_NAME, i_desc | d_0,
+	               (double)EmulMode::Count - delta, (double)EmulMode::Count + delta);
 
 	// Play mode
-	ports.add_port(c_desc, PLAY_MODE_STR, i_desc | d_0, -0.1, 4.1);
+	ports.add_port(c_desc, PLAY_MODE_NAME, i_desc | d_0,
+	               (double)PlayMode::Count - delta, (double)PlayMode::Count + delta);
 
 	// Tone
-	ports.add_port(c_desc, TONE_TIME_STR, r_desc | d_min,
+	ports.add_port(c_desc, TONE_TIME_NAME, r_desc | d_min,
 	               TONE_TIME_L, TONE_TIME_U);
-	ports.add_port(c_desc, TONE_DETUNE_STR, r_desc | d_0,
+	ports.add_port(c_desc, TONE_DETUNE_NAME, r_desc | d_0,
 	               TONE_DETUNE_L, TONE_DETUNE_U);
-	ports.add_port(c_desc, TONE_TRANSPOSE_STR, i_desc | d_0,
-	               TONE_TRANSPOSE_L - 0.1f, TONE_TRANSPOSE_U + 0.1);
-	ports.add_port(c_desc, TONE_SPREAD_STR, i_desc | d_0,
+	ports.add_port(c_desc, TONE_TRANSPOSE_NAME, i_desc | d_0,
+	               TONE_TRANSPOSE_L - delta, TONE_TRANSPOSE_U + delta);
+	ports.add_port(c_desc, TONE_SPREAD_NAME, r_desc | d_0,
 	               TONE_SPREAD_L, TONE_SPREAD_U);
 
 	// Noise
-	ports.add_port(c_desc, NOISE_TIME_STR, r_desc | d_0,
+	ports.add_port(c_desc, NOISE_TIME_NAME, r_desc | d_0,
 	               NOISE_TIME_L, NOISE_TIME_U);
-	ports.add_port(c_desc, NOISE_PERIOD_STR, i_desc | d_middle,
-	               NOISE_PERIOD_L - 0.1, NOISE_PERIOD_U + 0.1);
+	ports.add_port(c_desc, NOISE_PERIOD_NAME, i_desc | d_1,
+	               NOISE_PERIOD_L - delta, NOISE_PERIOD_U + delta);
 
 	// Noise Period Envelope
-	ports.add_port(c_desc, NOISE_PERIOD_ENV_ATTACK_STR, r_desc | d_0,
-	               NOISE_PERIOD_ENV_ATTACK_L, NOISE_PERIOD_ENV_ATTACK_U);
-	ports.add_port(c_desc, NOISE_PERIOD_ENV_TIME_STR, i_desc | d_middle,
+	ports.add_port(c_desc, NOISE_PERIOD_ENV_ATTACK_NAME, i_desc | d_0,
+	               NOISE_PERIOD_ENV_ATTACK_L - delta, NOISE_PERIOD_ENV_ATTACK_U + delta);
+	ports.add_port(c_desc, NOISE_PERIOD_ENV_TIME_NAME, r_desc | d_0,
 	               NOISE_PERIOD_ENV_TIME_L, NOISE_PERIOD_ENV_TIME_U);
 
 	// Amplitude envelope
-	ports.add_port(c_desc, AMP_ENV_ATTACK_TIME_STR, r_desc | d_0,
+	ports.add_port(c_desc, AMP_ENV_ATTACK_TIME_NAME, r_desc | d_0,
 	               AMP_ENV_ATTACK_TIME_L, AMP_ENV_ATTACK_TIME_U);
-	ports.add_port(c_desc, AMP_ENV_HOLD1_LEVEL_STR, r_desc | d_1,
+	ports.add_port(c_desc, AMP_ENV_HOLD1_LEVEL_NAME, r_desc | d_1,
 	               AMP_ENV_HOLD1_LEVEL_L, AMP_ENV_HOLD1_LEVEL_U);
-	ports.add_port(c_desc, AMP_ENV_INTER1_TIME_STR, r_desc | d_0,
+	ports.add_port(c_desc, AMP_ENV_INTER1_TIME_NAME, r_desc | d_0,
 	               AMP_ENV_INTER1_TIME_L, AMP_ENV_INTER1_TIME_U);
-	ports.add_port(c_desc, AMP_ENV_HOLD2_LEVEL_STR, r_desc | d_1,
+	ports.add_port(c_desc, AMP_ENV_HOLD2_LEVEL_NAME, r_desc | d_1,
 	               AMP_ENV_HOLD2_LEVEL_L, AMP_ENV_HOLD2_LEVEL_U);
-	ports.add_port(c_desc, AMP_ENV_INTER2_TIME_STR, r_desc | d_0,
+	ports.add_port(c_desc, AMP_ENV_INTER2_TIME_NAME, r_desc | d_0,
 	               AMP_ENV_INTER2_TIME_L, AMP_ENV_INTER2_TIME_U);
-	ports.add_port(c_desc, AMP_ENV_HOLD3_LEVEL_STR, r_desc | d_1,
+	ports.add_port(c_desc, AMP_ENV_HOLD3_LEVEL_NAME, r_desc | d_1,
 	               AMP_ENV_HOLD3_LEVEL_L, AMP_ENV_HOLD3_LEVEL_U);
-	ports.add_port(c_desc, AMP_ENV_DECAY_TIME_STR, r_desc | d_0,
+	ports.add_port(c_desc, AMP_ENV_DECAY_TIME_NAME, r_desc | d_0,
 	               AMP_ENV_DECAY_TIME_L, AMP_ENV_DECAY_TIME_U);
-	ports.add_port(c_desc, AMP_ENV_SUSTAIN_LEVEL_STR, r_desc | d_1,
+	ports.add_port(c_desc, AMP_ENV_SUSTAIN_LEVEL_NAME, r_desc | d_1,
 	               AMP_ENV_SUSTAIN_LEVEL_L, AMP_ENV_SUSTAIN_LEVEL_U);
-	ports.add_port(c_desc, AMP_ENV_RELEASE_STR, r_desc | d_0,
+	ports.add_port(c_desc, AMP_ENV_RELEASE_NAME, r_desc | d_0,
 	               AMP_ENV_RELEASE_L, AMP_ENV_RELEASE_U);
 
 	// Pitch envelope
-	ports.add_port(c_desc, PITCH_ENV_ATTACK_PITCH_STR, r_desc | d_0,
+	ports.add_port(c_desc, PITCH_ENV_ATTACK_PITCH_NAME, r_desc | d_0,
 	               PITCH_ENV_ATTACK_PITCH_L, PITCH_ENV_ATTACK_PITCH_U);
-	ports.add_port(c_desc, PITCH_ENV_TIME_STR, r_desc | d_0,
+	ports.add_port(c_desc, PITCH_ENV_TIME_NAME, r_desc | d_0,
 	               PITCH_ENV_TIME_L, PITCH_ENV_TIME_U);
 
 	// Arpegio
-	ports.add_port(c_desc, ARP_PITCH1_STR, r_desc | d_0,
-	               ARP_PITCH1_L, ARP_PITCH1_U);
-	ports.add_port(c_desc, ARP_PITCH2_STR, r_desc | d_0,
-	               ARP_PITCH2_L, ARP_PITCH2_U);
-	ports.add_port(c_desc, ARP_PITCH3_STR, r_desc | d_0,
-	               ARP_PITCH3_L, ARP_PITCH3_U);
-	ports.add_port(c_desc, ARP_FREQ_STR, r_desc | d_low,
-	               ARP_FREQ_L, ARP_FREQ_U);
-	ports.add_port(c_desc, ARP_REPEAT_STR, i_desc | d_0,
-	               ARP_REPEAT_L - 0.1, ARP_REPEAT_U + 0.1);
+	ports.add_port(c_desc, ARP_PITCH1_NAME, i_desc | d_0,
+	               ARP_PITCH1_L - delta, ARP_PITCH1_U + delta);
+	ports.add_port(c_desc, ARP_PITCH2_NAME, r_desc | d_0,
+	               ARP_PITCH2_L - delta, ARP_PITCH2_U + delta);
+	ports.add_port(c_desc, ARP_PITCH3_NAME, r_desc | d_0,
+	               ARP_PITCH3_L - delta, ARP_PITCH3_U + delta);
+	ports.add_port(c_desc, ARP_BEAT_DIVISOR_NAME, i_desc | d_low,
+	               ARP_BEAT_DIVISOR_L - delta, ARP_BEAT_DIVISOR_U + delta);
+	ports.add_port(c_desc, ARP_BEAT_MULTIPLIER_NAME, i_desc | d_low,
+	               ARP_BEAT_MULTIPLIER_L - delta, ARP_BEAT_MULTIPLIER_U + delta);
+	ports.add_port(c_desc, ARP_REPEAT_NAME, i_desc | d_0,
+	               ARP_REPEAT_L - delta, ARP_REPEAT_U + delta);
 
 	// Ring modulation
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL1_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL1_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL1_L, RING_MOD_WAVEFORM_LEVEL1_U);
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL2_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL2_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL2_L, RING_MOD_WAVEFORM_LEVEL2_U);
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL3_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL3_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL3_L, RING_MOD_WAVEFORM_LEVEL3_U);
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL4_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL4_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL4_L, RING_MOD_WAVEFORM_LEVEL4_U);
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL5_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL5_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL5_L, RING_MOD_WAVEFORM_LEVEL5_U);
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL6_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL6_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL6_L, RING_MOD_WAVEFORM_LEVEL6_U);
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL7_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL7_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL7_L, RING_MOD_WAVEFORM_LEVEL7_U);
-	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL8_STR, r_desc | d_1,
+	ports.add_port(c_desc, RING_MOD_WAVEFORM_LEVEL8_NAME, r_desc | d_1,
 	               RING_MOD_WAVEFORM_LEVEL8_L, RING_MOD_WAVEFORM_LEVEL8_U);
-	ports.add_port(c_desc, RING_MOD_MIRROR_STR, t_desc | d_1,
-	               RING_MOD_MIRROR_L - 0.1, RING_MOD_MIRROR_U + 0.1);
-	ports.add_port(c_desc, RING_MOD_SYNC_STR, t_desc | d_1,
-	               RING_MOD_SYNC_L - 0.1, RING_MOD_SYNC_U + 0.1);
-	ports.add_port(c_desc, RING_MOD_DETUNE_STR, r_desc | d_0,
+	ports.add_port(c_desc, RING_MOD_MIRROR_NAME, t_desc | d_1,
+	               RING_MOD_MIRROR_L - delta, RING_MOD_MIRROR_U + delta);
+	ports.add_port(c_desc, RING_MOD_SYNC_NAME, t_desc | d_1,
+	               RING_MOD_SYNC_L - delta, RING_MOD_SYNC_U + delta);
+	ports.add_port(c_desc, RING_MOD_DETUNE_NAME, r_desc | d_0,
 	               RING_MOD_DETUNE_L, RING_MOD_DETUNE_U);
-	ports.add_port(c_desc, RING_MOD_TRANSPOSE_STR, i_desc | d_0,
-	               RING_MOD_TRANSPOSE_L - 0.1, RING_MOD_TRANSPOSE_U + 0.1);
-	ports.add_port(c_desc, RING_MOD_FIXED_FREQUENCY_STR, i_desc | d_1,
-	               RING_MOD_FIXED_FREQUENCY_L, RING_MOD_FIXED_FREQUENCY_U);
-	ports.add_port(c_desc, RING_MOD_FIXED_VS_RELATIVE_STR, i_desc | d_0,
+	ports.add_port(c_desc, RING_MOD_TRANSPOSE_NAME, i_desc | d_0,
+	               RING_MOD_TRANSPOSE_L - delta, RING_MOD_TRANSPOSE_U + delta);
+	ports.add_port(c_desc, RING_MOD_FIXED_FREQUENCY_NAME, i_desc | d_1,
+	               RING_MOD_FIXED_FREQUENCY_L - delta, RING_MOD_FIXED_FREQUENCY_U + delta);
+	ports.add_port(c_desc, RING_MOD_FIXED_VS_RELATIVE_NAME, r_desc | d_1,
 	               RING_MOD_FIXED_VS_RELATIVE_L, RING_MOD_FIXED_VS_RELATIVE_U);
 
 	// Pitch LFO
-	ports.add_port(c_desc, LFO_FREQ_STR, r_desc | d_1,
+	ports.add_port(c_desc, LFO_FREQ_NAME, r_desc | d_low,
 	               LFO_FREQ_L, LFO_FREQ_U);
-	ports.add_port(c_desc, LFO_DELAY_STR, r_desc | d_0,
+	ports.add_port(c_desc, LFO_DELAY_NAME, r_desc | d_0,
 	               LFO_DELAY_L, LFO_DELAY_U);
-	ports.add_port(c_desc, LFO_DEPTH_STR, r_desc | d_0,
+	ports.add_port(c_desc, LFO_DEPTH_NAME, r_desc | d_0,
 	               LFO_DEPTH_L, LFO_DEPTH_U);
 
 	// Portamento
-	ports.add_port(c_desc, PORTAMENTO_TIME_STR, r_desc | d_0,
+	ports.add_port(c_desc, PORTAMENTO_TIME_NAME, r_desc | d_0,
 	               PORTAMENTO_TIME_L, PORTAMENTO_TIME_U);
 
 	// Gain
-	ports.add_port(c_desc, GAIN_STR, r_desc | d_1, GAIN_L, GAIN_U);
+	ports.add_port(c_desc, GAIN_NAME, r_desc | d_1, GAIN_L, GAIN_U);
 
 	// Pan
-	ports.add_port(c_desc, PAN0_STR, r_desc | d_middle,
+	ports.add_port(c_desc, PAN0_NAME, r_desc | d_middle,
 	               PAN0_L, PAN0_U);
-	ports.add_port(c_desc, PAN1_STR, r_desc | d_low,
+	ports.add_port(c_desc, PAN1_NAME, r_desc | d_low,
 	               PAN1_L, PAN1_U);
-	ports.add_port(c_desc, PAN2_STR, r_desc | d_high,
+	ports.add_port(c_desc, PAN2_NAME, r_desc | d_high,
 	               PAN2_L, PAN2_U);
 
-	// Pitch wheel range
-	ports.add_port(c_desc, PITCH_WHEEL_STR, i_desc | d_0,
-	               PITCH_WHEEL_L - 0.1, PITCH_WHEEL_U + 0.1);
+	// Control
+	ports.add_port(c_desc, PITCH_WHEEL_NAME, i_desc | d_middle,
+	               PITCH_WHEEL_L - delta, PITCH_WHEEL_U + delta);
 
-	// Emulation mode
-	ports.add_port(c_desc, EMUL_MODE_STR, i_desc | d_0, -0.1, 1.1);
+	ports.add_port(c_desc, VELOCITY_SENSITIVITY_NAME, r_desc | d_middle,
+	               VELOCITY_SENSITIVITY_L, VELOCITY_SENSITIVITY_U);
+
+	ports.add_port(c_desc, MODULATION_SENSITIVITY_NAME, r_desc | d_middle,
+	               MODULATION_SENSITIVITY_L, MODULATION_SENSITIVITY_U);	
 
 	//////////////////
 	// Audio output //
